70d98993ab8fe6a6ac572167bd7f38109494731c
gessilt <henriquefreitas2710@gmail.com>
2021-10-01 09:12:45 -0300

    feat: Vue implementation

--- database/migrations/2021_09_23_100423_create_users_table.php
+++ database/migrations/2021_09_23_100423_create_users_table.php
@@ -25,7 +25,9 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
-            $table->timestampTz($precision = 0);
+  $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
+                     $table->timestampTz($precision = 0);
         });
     }
 
