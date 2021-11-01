2fb68f497c5dc43169daa07fbe67ee0e02074558
gessilt <henriquefreitas2710@gmail.com>
2021-11-01 10:49:24 -0300

    feat: novos seeders

---
+++ database/seeders/LevelSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class LevelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('levels')->insert([
+            'title' => Str::random(3),
+            'description'=> Str::random(10),
+            'experience_required' => 1
+        ]);
+    }
+}
