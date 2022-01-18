8e7f47db548a94a8ba2e5da097fd52e03bf89348
gessilt <henriquefreitas2710@gmail.com>
2022-01-18 07:59:52 -0300

    feat: new page

---
+++ resources/js/Pages/Login.vue
@@ -0,0 +1,26 @@
+<template> 
+<div>
+    <form> 
+        <label for='email'> Seu email: </label>
+        <input type='text' placeholder='Seu email' v-model='form.email' name='email' id='email'>
+        <label for='password'> Sua senha: </label>
+        <input type='password' placeholder='Sua senha' v-model='form.password' name='password' id='password'>
+        <button type='submit'> Logar </button>
+    </form>
+</div>
+</template> 
+
+
+<script>
+
+export default {
+    data: () => {
+        return {
+            form: {
+                email: null,
+                password: null,
+            }
+        }
+    },
+}
+</script>
