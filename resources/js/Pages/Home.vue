66f9c80dbf188358c0a7aee44cf42abc72255482
gessilt <henriquefreitas2710@gmail.com>
2022-01-19 21:25:55 -0300

    feat: página home

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<template> 
+    <div>  
+        <div>
+            Sinto essa dor que causei no meu passado
+        </div>
+
+    </div>
+</template>
+
+
+<script> 
+
+    export default {}
+
+</script>
