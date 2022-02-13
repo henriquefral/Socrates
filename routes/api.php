fa3efa2e64b0b9fdbe4656807f0c787f53c5b996
gessilt <henriquefreitas2710@gmail.com>
2022-02-13 16:26:02 -0300

    feat: entidades
    Controllers das entidades foram adicionados na rota de APIs pararequisitar na página Vue3 e gerar o efeito de atualização simultânea.

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ParchmentController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TomeController;
 use App\Http\Controllers\ExamController;
+use App\Http\Controllers\UserController;
 
 
 /*
