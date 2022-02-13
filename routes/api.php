0aad063fd591159077efa5ec1e8e4aff4d583741
gessilt <henriquefreitas2710@gmail.com>
2022-02-13 16:22:26 -0300

    refactor

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\ParchmentController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TomeController;
+use App\Http\Controllers\ExamController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +26,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('api')->group(function (){
     Route::resource('notes',NoteController::class);
+    Route::resource('tasks',TaskController::class);
+    Route::resource('tomes',TomeController::class);
+    Route::resource('parchments',ParchmentController::class);
+    Route::resource('Exams',ExamController::class);
 });
