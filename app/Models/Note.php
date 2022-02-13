f847c078b2af3d5ad86cef60f9fcb2d3c27679b2
gessilt <henriquefreitas2710@gmail.com>
2022-02-13 15:38:03 -0300

    feat: fillable
    Adicionei a cada model seus campos que devem ser preenchidos

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -9,6 +9,18 @@ class Note extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'hightlights',
+        'date'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
